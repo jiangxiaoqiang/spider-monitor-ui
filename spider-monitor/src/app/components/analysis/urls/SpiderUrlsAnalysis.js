47225b12cc1416f4e5f8c18012e5d22770d07f28
jiangxiaoqiang <jiangtingqiang@gmail.com>
2019-03-01 14:09:16 +0800

    mod:添加URL统计图

--- spider-monitor/src/app/components/analysis/urls/SpiderUrlsAnalysis.js
+++ spider-monitor/src/app/components/analysis/urls/SpiderUrlsAnalysis.js
@@ -4,11 +4,13 @@ import {
   Geom,
   Axis,
   Tooltip,
+  Legend
 } from "bizcharts";
 import { getUrlsAnalysisImpl } from '../../../service/UrlsAnalysisService';
 import { connect } from 'react-redux';
 import {getUrlsAnalysis} from '../../../action/SpiderUrlsAnalysisAction';
-
+import DataSet from "@antv/data-set";
+import _ from 'lodash';
 
 class SpiderUrlsAnalysis extends Component {
 
@@ -18,47 +20,86 @@ class SpiderUrlsAnalysis extends Component {
 
   render() {
     const urlsAnalysis = this.props.urlsAnalysis;
-    const data = urlsAnalysis;
+    //const data = urlsAnalysis;
     console.log("urlsAnalysis:" + JSON.stringify(urlsAnalysis))
-    const cols = {
-      "unfinishedCount": {
-        min: 0
+
+    var result = _.chain(urlsAnalysis)
+    .groupBy("spiderName")
+    .map(function(currentItem){
+      return {
+        name: "1",
+        value: "2"
+      }
+    })
+    .value;
+
+
+    const data = [
+      {
+        analysisTimestamp: "Jan",
+        Tokyo: 7.0,
+        London: 3.9
       },
-      analysisTimestamp: {
+      {
+        analysisTimestamp: "Feb",
+        Tokyo: 6.9,
+        London: 4.2
+      }
+    ];
+    const ds = new DataSet();
+    const dv = ds.createView().source(data);
+    dv.transform({
+      type: "fold",
+      fields: ["Tokyo", "London"],
+      // 展开字段集
+      key: "city",
+      // key字段
+      value: "temperature" // value字段
+    });
+    console.log(dv);
+    const cols = {
+      month: {
         range: [0, 1]
       }
     };
     return (
-      <div className="container">
-        <div>
-          <Chart height={500} data={data} scale={cols} forceFit>
-            <span className='main-title'>
-              抓取地址统计
-            </span>
-            <Axis title="time" name="时间" />
-            <Axis name="书籍总数" />
-            <Tooltip
-              crosshairs={{
-                type: "y"
-              }}
-            />
-            <Geom type="line" position="analysisTimestamp*unfinishedCount" size={2} />
-            <Geom
-              type="point"
-              position="analysisTimestamp*unfinishedCount"
-              size={4}
-              shape={"circle"}
-              style={{
-                stroke: "#fff",
-                lineWidth: 1
-              }}
-            />
-          </Chart>
-        </div>
+      <div>
+        <Chart height={400} data={dv} scale={cols} forceFit>
+          <Legend />
+          <Axis name="month" />
+          <Axis
+            name="temperature"
+            label={{
+              formatter: val => `${val}°C`
+            }}
+          />
+          <Tooltip
+            crosshairs={{
+              type: "y"
+            }}
+          />
+          <Geom
+            type="line"
+            position="analysisTimestamp*temperature"
+            size={2}
+            color={"city"}
+          />
+          <Geom
+            type="point"
+            position="analysisTimestamp*temperature"
+            size={4}
+            shape={"circle"}
+            color={"city"}
+            style={{
+              stroke: "#fff",
+              lineWidth: 1
+            }}
+          />
+        </Chart>
       </div>
     );
   }
-}
+  }
 
 const mapStateToProps = state => ({
   urlsAnalysis: state.urlsAnalysis
